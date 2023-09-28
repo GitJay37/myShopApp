6dd457ec74b34ed5f078da9ed9fb487884aefcbd
Jay <jeisondejesus@hotmail.es>
2023-09-28 13:55:22 -0500

    Constructor Demo

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -8,18 +8,19 @@ public class ShopApp {
         System.out.println("Welcome to Duke Choice Shop");
 
         Customer c1 = new Customer("Pinky", 34);
+
+        //c1.setName("Pinky");
+        //c1.setSize("S");
+
         Clothing item1 = new Clothing("Blue Jacktet", 20.9, "M");
         Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
 
         Clothing[] items = {item1, item2, new Clothing("Green Scarf", 5.0,"S"), new Clothing("Blue T-Shirt",10.5, "S" )};
 
-        //c1.setName("Pinky");
-        //c1.setSize("S");
-
         //System.out.println("Item 1"+","+ item1.description + ","+ item1.price + "," + item1.size );
         //System.out.println("Item 2"+","+ item2.description + ","+ item2.price + "," + item2.size );
         //total = ( item1.price + item2.price * 2 ) * (1 + tax);
-        //System.out.println("Total = " +total);
+
 
         int measurement = 8;
         c1.addItems(items);
