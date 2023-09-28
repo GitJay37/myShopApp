5aee05c061f2efbd931223014de1941d23071500
Jay <jeisondejesus@hotmail.es>
2023-09-28 13:38:35 -0500

    Revert "Constructor Demo"
    This reverts commit 5ae9160e

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -7,30 +7,14 @@ public class ShopApp {
 
         System.out.println("Welcome to Duke Choice Shop");
 
-        Customer c1 = new Customer("Pinky", 14);
-        Clothing item1 = new Clothing();
-        Clothing item2 = new Clothing();
+        Customer c1 = new Customer("Pinky", 34);
+        Clothing item1 = new Clothing("Blue Jacktet", 20.9, "M");
+        Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
 
-        Clothing[] items = {item1, item2, new Clothing(), new Clothing()};
+        Clothing[] items = {item1, item2, new Clothing("Green Scarf", 5.0,"S"), new Clothing("Blue T-Shirt",10.5, "S" )};
 
         //c1.setName("Pinky");
         //c1.setSize("S");
-
-        item1.setDescription("Blue Jacktet");
-        item1.setPrice(20.9);
-        item1.setSize("M");
-
-        item2.setDescription("Orange T-Shirt");
-        item2.setPrice(10.5);
-        item2.setSize("S");
-
-        items[2].setDescription("Green Scarf");
-        items[2].setPrice(5.0);
-        items[2].setSize("S");
-
-        items[3].setDescription("Blue T-Shirt");
-        items[3].setPrice(10.5);
-        items[3].setSize("S");
 
         //System.out.println("Item 1"+","+ item1.description + ","+ item1.price + "," + item1.size );
         //System.out.println("Item 2"+","+ item2.description + ","+ item2.price + "," + item2.size );
